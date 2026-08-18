549f643f10ea67a75171feaed988c1fbb94def34
9369702788 <67014896+9369702788@users.noreply.github.com>
2026-08-18 23:04:22 +0300

    Add files via upload

--- android/app/src/main/kotlin/com/example/wirdi/WirdiWidgetProvider.kt
+++ android/app/src/main/kotlin/com/example/wirdi/WirdiWidgetProvider.kt
@@ -1,5 +1,4 @@
 package com.example.wirdi
-
 import android.appwidget.AppWidgetManager
 import android.content.Context
 import android.content.SharedPreferences
@@ -10,13 +9,11 @@ class WirdiWidgetProvider : HomeWidgetProvider() {
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray, widgetData: SharedPreferences) {
         val packageName = context.packageName
         val layoutId = context.resources.getIdentifier("wirdi_widget_layout", "layout", packageName)
-        
         for (appWidgetId in appWidgetIds) {
             val views = RemoteViews(packageName, layoutId).apply {
                 val nameId = context.resources.getIdentifier("widget_next_prayer_name", "id", packageName)
                 val timeId = context.resources.getIdentifier("widget_next_prayer_time", "id", packageName)
                 val hadithId = context.resources.getIdentifier("widget_hadith_arabic", "id", packageName)
-                
                 setTextViewText(nameId, widgetData.getString("next_prayer_name", "Next Prayer"))
                 setTextViewText(timeId, widgetData.getString("next_prayer_time", "--:--"))
                 setTextViewText(hadithId, widgetData.getString("hadith_arabic", "Wirdi"))
--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -1,4 +1,4 @@
-import 'package:file_picker/file_picker.dart';
+
 import '../../core/services/backup_service.dart';
 import 'dart:async';
 
